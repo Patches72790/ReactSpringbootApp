d11ba5221942a3d5000c3424c87bb8ff7bf907e4
Patrick <patches72790@gmail.com>
2022-09-26 19:01:16 -0500

    added post and get methods for character controller

--- src/main/java/com/characterviewer/CharacterRepository.java
+++ src/main/java/com/characterviewer/CharacterRepository.java
@@ -3,7 +3,9 @@ package com.characterviewer;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "characters", path = "characters")
 public interface CharacterRepository extends CrudRepository<Character, Long> {
     @Override
     Optional<Character> findById(Long id);
